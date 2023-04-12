6ecdf6fbbc5e7d9ceb139a9c29625fa2bb896db0
abdelaziz <abdelazizsaleh1999@gmail.com>
2023-04-12 22:05:02 +0200

    second commit

---
+++ IdentitySystem/Controllers/AccountController.cs
@@ -0,0 +1,29 @@
+﻿using IdentitySystem.ViewModels;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace IdentitySystem.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly SignInManager<IdentityUser> signInManager;
+        private readonly UserManager<IdentityUser> userManager;
+        public AccountController(SignInManager<IdentityUser> _signInManager, UserManager<IdentityUser> _userManager)
+        {
+            this.signInManager = _signInManager;
+            this.userManager = _userManager;
+        }
+        [HttpGet]
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Register(RegisterViewModel user)
+        {
+
+            return View();
+        }
+    }
+}
